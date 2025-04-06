a7e1dd93c92f8cee65d4f2f6fb088255337fdab5
Eric Arellano <ericarellano@me.com>
2025-04-06 13:55:35 -0400

    Refactor Success component

--- src/lib/components/travelTimes/TravelTimes.svelte
+++ src/lib/components/travelTimes/TravelTimes.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
 	import Card from '../Card.svelte';
 	import Loading from './Loading.svelte';
+	import Success from './Success.svelte';
 	import travelTimesState from '$lib/state/travelTimes.svelte';
-	import Church from './places/Church.svelte';
-	import FarmersMarket from './places/FarmersMarket.svelte';
-	import Fractal from './places/Fractal.svelte';
-	import Park from './places/Park.svelte';
-	import Partner from './places/Partner.svelte';
-	import SubwayStop from './places/SubwayStop.svelte';
-	import Work from './places/Work.svelte';
 </script>
 
 {#if travelTimesState.status !== 'unset'}
@@ -16,15 +10,7 @@
 		{#if travelTimesState.status === 'loading'}
 			<Loading />
 		{:else if travelTimesState.status === 'success'}
-			<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-				<Work {...travelTimesState.result.work} />
-				<Partner {...travelTimesState.result.partner} />
-				<SubwayStop {...travelTimesState.result.subwayStop} />
-				<Park {...travelTimesState.result.park} />
-				<FarmersMarket {...travelTimesState.result.farmersMarket} />
-				<Fractal {...travelTimesState.result.fractal} />
-				<Church {...travelTimesState.result.church} />
-			</div>
+			<Success {...travelTimesState.result} />
 		{:else}
 			<p>Error: {travelTimesState.error}</p>
 		{/if}
