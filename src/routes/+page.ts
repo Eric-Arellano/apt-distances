6b8931aa59d44cc6053d77a777cc3aa8f1a33dad
Eric Arellano <ericarellano@me.com>
2025-04-06 14:11:02 -0400

    Remove load function

--- src/routes/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { TravelTimes } from '$lib/types';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('/api/travel-times');
-	const travelTimes: TravelTimes = await res.json();
-	return { travelTimes };
-};
