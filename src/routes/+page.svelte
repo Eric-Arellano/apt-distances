8050bc8d35583176d274a0ce13fa547e97b775b1
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2025-04-06 17:15:43 -0400

    Add basic frontend for site login (#15)
    The auth scheme isn't actually implemented yet. You can enter any
    password and it will be accepted. This is only to get the design
    implemented.
    
    The design isn't fully finished because we still need some type of
    loading & error state visualization.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Address from '$lib/components/address';
+	import AddressCard from '$lib/components/address';
 	import TravelTimes from '$lib/components/travelTimes/index.js';
 	import Neighborhoods from '$lib/components/Neighborhoods.svelte';
 	import OtherCriteria from '$lib/components/criteria';
@@ -9,7 +9,7 @@
 	<div class="container mx-auto max-w-6xl px-4">
 		<h1 class="mb-6 text-center text-3xl font-bold md:text-left">NYC apartment travel times</h1>
 		<Neighborhoods />
-		<Address />
+		<AddressCard />
 		<TravelTimes />
 		<OtherCriteria />
 	</div>
