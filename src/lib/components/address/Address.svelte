77e7b9b539f9f3f56b410973090eefba2b09481e
Eric Arellano <ericarellano@me.com>
2025-04-06 15:47:12 -0400

    Disable multiple requests

--- src/lib/components/address/Address.svelte
+++ src/lib/components/address/Address.svelte
@@ -4,6 +4,15 @@
 	import { isValidAddress } from './addressUtils.js';
 
 	let address = $state('');
+	let isRequestInFlight = $state(false);
+
+	function handleClick() {
+		if (isRequestInFlight) return;
+		isRequestInFlight = true;
+		travelTimesRequest.task = getTravelTimes().finally(() => {
+			isRequestInFlight = false;
+		});
+	}
 </script>
 
 <Card title="Street address">
@@ -16,11 +25,10 @@
 			/>
 		</div>
 		<div>
-			<!-- TODO: disabled should not allow multiple promises -->
 			<button
 				class="w-full cursor-pointer rounded-md bg-blue-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed disabled:bg-blue-400 disabled:opacity-50 disabled:hover:bg-blue-400 sm:w-auto"
-				disabled={!isValidAddress(address)}
-				onclick={() => (travelTimesRequest.task = getTravelTimes())}
+				disabled={!isValidAddress(address) || isRequestInFlight}
+				onclick={handleClick}
 			>
 				Calculate travel times
 			</button>
