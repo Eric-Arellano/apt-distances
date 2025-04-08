a6750eafdfe1b3c908c3141b77b110b96aaa1954
Eric Arellano <ericarellano@me.com>
2025-04-07 23:07:31 -0400

    Add transit route

--- src/routes/api/experiment/+server.ts
+++ src/routes/api/experiment/+server.ts
@@ -1,13 +1,11 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-import { computeRoutes } from '$lib/server/gmaps';
+import { computeTransitRoute } from '$lib/server/gmaps';
 
 export const GET: RequestHandler = async () => {
-	const result = await computeRoutes({
+	const result = await computeTransitRoute({
 		origin: '410 10th Ave, New York, NY',
-		dest: '1 Madison Ave, New York, NY 10010',
-		walk: true,
-		bike: true
+		dest: '1 Madison Ave, New York, NY 10010'
 	});
 	return json(result);
 };
