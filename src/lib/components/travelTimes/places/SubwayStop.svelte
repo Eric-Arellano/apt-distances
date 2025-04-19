6f4941baf189bd847437911cfa63d8954f6f65ea
Eric Arellano <ericarellano@me.com>
2025-04-19 13:30:54 -0400

    DRY goalStatus

--- src/lib/components/travelTimes/places/SubwayStop.svelte
+++ src/lib/components/travelTimes/places/SubwayStop.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
 	import DestinationCard from '../DestinationCard.svelte';
-	import type { TravelTimes, GoalStatus } from '$lib/types';
+	import type { TravelTimes } from '$lib/types';
+	import { goalStatus } from '../utils';
 
 	let { closest, walk }: TravelTimes['subwayStop'] = $props();
 
 	const idealMinutes = 7;
 	const maxMinutes = 12;
-
-	function goalStatus(): GoalStatus {
-		if (walk.timeMinutes <= idealMinutes) {
-			return 'met';
-		}
-		if (walk.timeMinutes <= maxMinutes) {
-			return 'partial';
-		}
-		return 'unmet';
-	}
 </script>
 
 <DestinationCard
@@ -24,5 +15,5 @@
 	routes={[
 		`🚶 ${walk.timeMinutes} minutes to ${closest.name} with ${closest.lines.join(', ')} (${walk.distanceMiles} miles)`
 	]}
-	goalStatus={goalStatus()}
+	goalStatus={goalStatus([walk], { idealMinutes, maxMinutes })}
 />
--- src/lib/components/travelTimes/places/Work.svelte
+++ src/lib/components/travelTimes/places/Work.svelte
@@ -1,28 +1,17 @@
 <script lang="ts">
 	import DestinationCard from '../DestinationCard.svelte';
-	import type { TravelTimes, GoalStatus } from '$lib/types';
-	import { bikeRoute, transitRoute, walkRoute } from '../utils';
+	import type { TravelTimes } from '$lib/types';
+	import { bikeRoute, goalStatus, transitRoute, walkRoute } from '../utils';
 
 	let { walk, bike, transit }: TravelTimes['work'] = $props();
 
 	const idealMinutes = 15;
 	const maxMinutes = 25;
-
-	function goalStatus(): GoalStatus {
-		const minTime = Math.min(walk.timeMinutes, bike.timeMinutes, transit.timeMinutes);
-		if (minTime <= idealMinutes) {
-			return 'met';
-		}
-		if (minTime <= maxMinutes) {
-			return 'partial';
-		}
-		return 'unmet';
-	}
 </script>
 
 <DestinationCard
 	title="Work 👨🏼‍💻"
 	goal="Must be within {maxMinutes} minutes; ideally within {idealMinutes} minutes"
 	routes={[transitRoute(transit), bikeRoute(bike), walkRoute(walk)]}
-	goalStatus={goalStatus()}
+	goalStatus={goalStatus([walk, bike, transit], { idealMinutes, maxMinutes })}
 />
