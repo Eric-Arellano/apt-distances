659e4ade2a5f46b5e97bb03998810d57d8d51419
Eric Arellano <ericarellano@me.com>
2025-04-20 14:07:32 -0400

    DRY Loading and Error

---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	interface Props {
+		message: string;
+	}
+
+	const { message }: Props = $props();
+</script>
+
+<div class="flex items-center justify-center">
+	<div class="h-10 w-10 animate-spin rounded-full border-4 border-blue-200 border-t-blue-500"></div>
+	<span class="ml-3 text-lg text-gray-600">{message}</span>
+</div>
--- src/lib/components/travelTimes/Loading.svelte
+++
@@ -1,4 +0,0 @@
-<div class="flex items-center justify-center">
-	<div class="h-10 w-10 animate-spin rounded-full border-4 border-blue-200 border-t-blue-500"></div>
-	<span class="ml-3 text-lg text-gray-600">Calculating travel times...</span>
-</div>
